c5e842b7e9c68dd500fd630ef3b0583b2d4e9024
Soumyadeepdas2 <soumyadeepdas26xy@gmail.com>
2026-09-08 23:43:35 +0530

    Pushed via PUSHkar

---
+++ GeeksforGeeks/python-conditionals/the-else-statement/solution.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    // code here
+    if(a>100){
+        cout<<"Big";
+    }else{
+        cout<<"Small";
+    }
+
+    return 0;
+}
