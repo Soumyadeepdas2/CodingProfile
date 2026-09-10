e7247cc45b50e341a7bd39ec3b2a99389b3c67a1
Soumyadeepdas2 <soumyadeepdas26xy@gmail.com>
2026-09-10 22:37:23 +0530

    Pushed via PUSHkar

---
+++ LeetCode/Tree/count-nodes-equal-to-average-of-subtree/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    void isInRange(int n) {
+        // you don't need to take input
+        // here just write code which print correct answer according to n
+        if(n==1){
+            cout<<"one"<<endl;
+        }else if(n==2){
+            cout<<"two"<<endl;
+        }
+        else if(n==3){
+            cout<<"three"<<endl;
+        }else if(n==4){
+            cout<<"four"<<endl;
+        }else if(n==5){
+            cout<<"five"<<endl;
+        }else if(n==6){
+            cout<<"six"<<endl;
+        }else if(n==7){
+            cout<<"seven"<<endl;
+        }else if(n==8){
+            cout<<"eight"<<endl;
+        }else if(n==9){
+            cout<<"nine"<<endl;
+        }else if(n==10){
+            cout<<"ten"<<endl;
+        }else{
+            cout<<"not in range"<<endl;
+        }
+    }
+};
