f5e151b949fab05f60447c3f244d3a28e1090baa
Soumyadeepdas2 <soumyadeepdas26xy@gmail.com>
2026-09-20 23:40:09 +0530

    Pushed via PUSHkar

---
+++ Codeforces/brute force/team/solution.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    
+    int answer = 0;
+    for(int i = 0; i < n; i++){
+        int count = 0;
+        int a,b,c;
+        cin >>a>>b>>c;
+      if(a==1){
+        count++;
+      }if(b==1){
+        count++;
+      }if(c==1){
+        count++;
+      }
+      if(count>1){
+        answer++;
+      }
+    }
+    cout << answer;
+    return 0;
+}
