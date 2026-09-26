1a49f76e3f1be7af538bea90e54fabd29aecc348
Soumyadeep Das <soumyadeepdas26xy@gmail.com>
2026-09-26 21:17:07 +0530

    Pushed via PUSHkar

---
+++ Codeforces/implementation/is-your-horseshoe-on-the-other-hoof/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<int> colors;
+
+    for (int i = 0; i < 4; i++) {
+        int x;
+        cin >> x;
+        colors.insert(x);
+    }
+
+    cout << 4 - colors.size() << endl;
+
+    return 0;
+}
